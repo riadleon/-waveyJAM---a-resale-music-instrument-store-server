e1154e2ed9c4fb5ca97faeab950037551c709488
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-25 03:01:06 +0600

    category id problem solved

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ async function run() {
             res.send(categories);
         });
         app.get('/categories/:id', async (req, res) => {
-            const id = req.params.category_id;
-            const query = { _id: id };
+            const id = req.params.id;
+            const query = { id: id };
             const product = await productCollection.findOne(query);
             res.send(product);
         })
